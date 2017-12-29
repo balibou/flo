a394e36a1caef7b505e85c4244466b3a5b01d208
Benjamin Cherion <benjamin.cherion@canal-plus.com>
2017-12-29 15:34:44 +0100

    up

---
+++ bump2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#####################################
+# $ git checkout master
+# Switched to branch 'master'
+# $ git merge --no-ff hotfix-1.2.1
+# Merge made by recursive.
+# (Summary of changes)
+# $ git tag -a 1.2.1
+#####################################
+
+
+
+# checks if branch has something pending
+function parse_git_dirty() {
+  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+}
+
+# gets the current git branch
+function parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+# get last commit hash prepended
+function parse_git_hash() {
+  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)//"
+}
+
+# function parse_version() {
+#     # '/(?:(?<major>(?:0|[1-9](?:(?:0|[1-9])+)*))[.](?<minor>(?:0|[1-9](?:(?:0|[1-9])+)*))[.](?<patch>(?:0|[1-9](?:(?:0|[1-9])+)*)))$/'
+# }
+
+# DEMO
+if [ -f VERSION ]; then
+    GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+    GIT_VERSION=$(echo $GIT_BRANCH | sed 's/.*hotfix-\([^ ]*\).*/\1/')
+    git checkout master
+    git merge --no-ff $GIT_BRANCH
+    git tag -a $GIT_VERSION
+
+else
+    echo "Could not find a VERSION file"
+fi
